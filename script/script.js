3c1130d6d80fb37806e6c7fae600aa23496bfee4
Stuart Myers <stuart.myers@gmail.com>
2019-12-19 16:06:10 +0800

    You can now "win" the game

--- script/script.js
+++ script/script.js
@@ -1,1 +1,15 @@
 console.log("Script Loaded!")
+
+var mainInputBox = document.querySelector('#mainInput');
+
+console.log(mainInputBox);
+
+var doIWinTheGame = function(event) {
+    // console.log('no');
+    var inputValue = this.value;
+
+    if (inputValue === "winner") {
+        alert('You win the game!');
+    }
+};
+mainInputBox.addEventListener('change', doIWinTheGame);
