5102fd98db6d4f620458668a6c7f11b50326d2f7
Stuart Myers <stuart.myers@gmail.com>
2019-12-20 15:14:09 +0800

    A bug with missing letters. Attempt to fix.

--- script/script.js
+++ script/script.js
@@ -2,17 +2,23 @@ console.log("Script Loaded!")
 
 var mainInputBox = document.querySelector('#mainInput');
 var wordDisplay = document.querySelector('#wordDisplay');
+var correctlyTypedDisplay = document.querySelector('#correctlyTypedPortion');
+var wronglyTypedDisplay = document.querySelector('#wronglyTypedPortion');
+var completeWordHint = document.querySelector('#completeWord');
 var beginGameButton = document.querySelector('#beginGame');
 var scoreText = document.querySelector('#scoreText');
 var livesText = document.querySelector('#livesText');
 
-// var wordlistArray = ["wordOne", "wordTwo", "wordThree", "wordFour", "wordFive", "wordSix"];
-var activeWord;
 var playerLives = 5;
 var gameOver = true;
 var score = 0;
+var mistakeMade = false
 
-// console.log(mainInputBox);
+var activeWord;
+var correctlyTypedPortion = "";
+var wronglyTypedPortion = "";
+var remainingToTypePortion = "";
+
 
 
 var checkWordMatched = function(event) {
@@ -20,21 +26,31 @@ var checkWordMatched = function(event) {
     var inputValue = this.value;
 
     // Check word partially matches what has been typed in so far.
-    if (inputValue === activeWord.slice(0, inputValue.length)) {
-        // console.log(inputValue + " ok");
+    if (inputValue === activeWord.slice(0, inputValue.length)) {       
+        remainingToTypePortion = remainingToTypePortion.slice(1);
+        correctlyTypedDisplay.textContent = inputValue;
+        wordDisplay.textContent = remainingToTypePortion;
+        mistakeMade = false;
+        wronglyTypedPortion = "";
+        wronglyTypedDisplay.textContent = wronglyTypedPortion;
     } else {
-        console.log(inputValue + " typo!");
+        // console.log(inputValue + " typo!");
         this.value = inputValue.slice(0, inputValue.length - 1);
         wrongLetterTyped();
     }
+
+    if (!inputValue) {
+        remainingToTypePortion = activeWord;
+    }
+    
 
     // if the input equals the full active word.
     if (inputValue === activeWord) {
         console.log("word matched!");
         this.value = "";
-        choosenewWord();
         score++;
         updateScore();
+        choosenewWord();
     } else {
         // console.log(inputValue + " is not matched.");
     }
@@ -43,15 +59,25 @@ var checkWordMatched = function(event) {
 
 var choosenewWord = function () {
     activeWord = "";
+    correctlyTypedDisplay.textContent = "";
+    mainInputBox.value = "";
     var newWordIndex = Math.floor(Math.random() * wordDictionary.length);
     activeWord = wordDictionary[newWordIndex];
+    remainingToTypePortion = activeWord;
     wordDisplay.textContent = activeWord;
+    completeWordHint.textContent = activeWord;
 }
 
 
 var wrongLetterTyped = function() {
-    playerLives--;
-    console.log("Lives left: " + playerLives);
+    if (!mistakeMade){
+        playerLives--;
+        mistakeMade = true;
+        console.log("Lives left: " + playerLives);
+    }
+    wronglyTypedPortion = remainingToTypePortion[0];
+    wordDisplay.textContent = remainingToTypePortion.slice(1);
+    wronglyTypedDisplay.textContent = wronglyTypedPortion;
     checkGameOver();
 }
 
@@ -73,8 +99,11 @@ var beginGame = function () {
         playerLives = 5;
         console.log('game begin');
         mainInputBox.removeAttribute('disabled');
+        wronglyTypedPortion = "";
+        wronglyTypedDisplay.textContent = wronglyTypedPortion;
         choosenewWord();
         updateScore();
+        mainInputBox.focus();
     }
 }
 
@@ -83,6 +112,7 @@ var updateScore = function () {
     scoreText.textContent = score;
     livesText.textContent = playerLives;
 }
+
 
 
 mainInputBox.addEventListener('keyup', checkWordMatched);
