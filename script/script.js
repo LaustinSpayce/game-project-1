3306449b13d2d5617dfa363f8d010b35290f57cf
Stuart Myers <stuart.myers@gmail.com>
2019-12-20 11:56:20 +0800

    chooses ranodm word from dictionary to match.

--- script/script.js
+++ script/script.js
@@ -1,15 +1,34 @@
 console.log("Script Loaded!")
 
 var mainInputBox = document.querySelector('#mainInput');
+var wordDisplay = document.querySelector('#wordDisplay');
+
+// var wordlistArray = ["wordOne", "wordTwo", "wordThree", "wordFour", "wordFive", "wordSix"];
+var activeWord;
 
 console.log(mainInputBox);
 
-var doIWinTheGame = function(event) {
+
+var checkWordMatched = function(event) {
     // console.log('no');
     var inputValue = this.value;
 
-    if (inputValue === "winner") {
-        alert('You win the game!');
+    if (inputValue === activeWord) {
+        console.log("word matched!");
+        this.value = "";
+        choosenewWord();
+    } else {
+        // console.log(inputValue + " is not matched.");
     }
 };
-mainInputBox.addEventListener('change', doIWinTheGame);
+
+
+var choosenewWord = function () {
+    activeWord = "";
+    var newWordIndex = Math.floor(Math.random() * wordDictionary.length);
+    activeWord = wordDictionary[newWordIndex];
+    wordDisplay.textContent = activeWord;
+}
+
+mainInputBox.addEventListener('keyup', checkWordMatched);
+choosenewWord();
